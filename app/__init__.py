8698e7ee9b76a17d99bc9cb3bb069fc4d555707a
Ihor <skogrv@gmail.com>
2023-04-20 19:38:50 +0300

    feat: create models.py and login authorization for users

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,36 @@
-import flask 
+import flask
 from decouple import config
 from flask import Flask
+from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = flask.Flask(__name__)
-from app import views
-from .auth import routes
-app.register_blueprint(auth.routes.auth_bp)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 app.config.from_object(config("APP_SETTINGS"))
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "auth.login"
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+
+
+from app.auth.models import User 
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == int(user_id)).first()
+
 migrate = Migrate(app, db)
+
+from app.auth.views import auth_bp
+from app import views
+
+app.register_blueprint(auth.views.auth_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
---
+++ app/auth/models.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from flask_login import UserMixin
+from app import bcrypt, db
+
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = bcrypt.generate_password_hash(password)
+        self.created_on = datetime.now()
