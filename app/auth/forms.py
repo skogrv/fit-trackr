e999ebded2a2e83c7e07433c7f1d759804d7cd11
Ihor <skogrv@gmail.com>
2023-04-21 11:09:12 +0300

    fix: add DataRequired() for Registration form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(),
+    username = StringField('Username', validators=[DataRequired(),
                                                    Length(min=5, max=20)])
-    password = PasswordField('New Password', validators=[InputRequired(),
+    password = PasswordField('New Password', validators=[DataRequired()(),
                                                          Length(min=7, max=20),
                                                          EqualTo('confirm', message='Passwords must match'
                                                                  )])
-    confirm = PasswordField('Repeat Password')
+    confirm = PasswordField('Repeat Password', validators=[
+                            DataRequired(), EqualTo("password", message="Passwords must match")])
