a54e8e8b64026e81fcc659964909089c7279e234
Ihor <skogrv@gmail.com>
2023-04-29 20:56:29 +0300

    feat: add functionality to exercises
    - prevent adding new input fields
    - change the colors when clicking on buttons
    - add pattern to recorgnize empty strings (or non alphabetic strings)

--- app/core/static/core/script.js
+++ app/core/static/core/script.js
@@ -1,32 +1,16 @@
-function showAddExerciseForm() {
-    // Create a new table row
-    var newRow = document.createElement("tr");
-    newRow.id = "add-exercise-row";
+document.addEventListener("DOMContentLoaded", function () {
+    const exerciseBtn = document.querySelector(".exercice-btn")
 
-    // Create table cells for the form fields
-    var exerciseCell = document.createElement("td");
-
-    // Add input fields to the table cells
-    exerciseCell.innerHTML = '<input type="text" class="form-control" name="exercise" placeholder="Exercise">';
-
-    // Add the cells to the row
-    newRow.appendChild(exerciseCell);
-    
-    // Insert the new row before the first row in the table
-    var tableBody = document.getElementById("exercise-table-body");
-    tableBody.insertBefore(newRow, tableBody.firstChild); 
-
-    var inputField = newRow.querySelector("input");
-    inputField.addEventListener("keypress", function (event) {
-        // If "Enter" key is pressed, create a new exercise row
-        if (event.key === "Enter") {
-            addExerciseRow(inputField.value, newRow);
-            event.preventDefault();
-            inputField.value = "";
+    exerciseBtn.addEventListener("click", function () {
+        if (exerciseBtn.textContent == "Add") {
+            createInputForm();
         }
-    });
-    inputField.focus();
-}
+        else {
+            removeInputForm();
+
+        }
+    })
+})
 
 function addExerciseRow(exerciseName, newRow) {
     // Create a new table row
@@ -44,4 +28,59 @@ function addExerciseRow(exerciseName, newRow) {
     // Add the row to the table body
     var tableBody = document.getElementById("exercise-table-body");
     tableBody.insertBefore(exerciseRow, newRow.nextSibling);
+}
+
+function createInputForm() {
+    // Create a new table row
+    var newRow = document.createElement("tr");
+    newRow.id = "add-exercise-row";
+
+    // Create table cells for the form fields
+    var exerciseCell = document.createElement("td");
+
+    // Add input fields to the table cells
+    exerciseCell.innerHTML = '<input type="text" class="form-control exercise-input" name="exercise" placeholder="Exercise">';
+
+    // Add the cells to the row
+    newRow.appendChild(exerciseCell);
+
+    // Insert the new row before the first row in the table
+    var tableBody = document.getElementById("exercise-table-body");
+    tableBody.insertBefore(newRow, tableBody.firstChild);
+
+    // Change the color of the button once it is clicked
+    changeButton("X")
+
+    var inputField = newRow.querySelector(".exercise-input");
+    var pattern = /[a-zA-Z]/;
+    inputField.addEventListener("keypress", function (event) {
+        // If "Enter" key is pressed, create a new exercise row
+        if (event.key === "Enter" && pattern.test(inputField.value)) {
+            addExerciseRow(inputField.value, newRow);
+            event.preventDefault();
+            inputField.value = "";
+        }
+    });
+    inputField.focus();
+}
+
+function removeInputForm() {
+    var inputField = document.querySelector("#exercise-table-body");
+    var tableRow = inputField.children[0];
+    tableRow.remove();
+    changeButton("Add")
+}
+
+function changeButton(toChange) {
+    var exercise_btn = document.querySelector(".exercice-btn");
+    if (toChange === "X") {
+        exercise_btn.textContent = "X";
+        exercise_btn.classList.remove("btn-primary");
+        exercise_btn.classList.add("btn-danger");
+    }
+    else {
+        exercise_btn.textContent = "Add";
+        exercise_btn.classList.remove("btn-danger");
+        exercise_btn.classList.add("btn-primary");
+    }
 }
