374cf44da6f1f167a65a90ec481fc73fc487f597
Ihor <skogrv@gmail.com>
2023-05-04 18:21:07 +0300

    fix: fix displaying form.errors
    - move logic for handling validation to flask form, not view

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length, EqualTo
 from .models import User
+from app import bcrypt
+
 
 
 class RegistrationForm(FlaskForm):
@@ -9,10 +11,10 @@ class RegistrationForm(FlaskForm):
                                                    Length(min=5, max=20)])
     password = PasswordField('New Password', validators=[DataRequired(),
                                                          Length(min=7, max=20),
-                                                         EqualTo('confirm', message='Passwords must match'
-                                                                 )])
+                                                         ])
     confirm = PasswordField('Repeat Password', validators=[
-                            DataRequired(), EqualTo("password", message="Passwords must match")])
+                            DataRequired(), EqualTo('password', message='Passwords must match'
+                                                                 )])
 
     def validate(self, extra_validators=None):
         initial_validation = super(RegistrationForm, self).validate()
@@ -22,12 +24,22 @@ class RegistrationForm(FlaskForm):
         if user:
             self.username.errors.append("Username is already registered")
             return False
-        if self.password.data != self.confirm.data:
-            self.password.errors.append("Passwords must match")
-            return False
         return True
 
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
+
+    def validate(self, extra_validators=None):
+        initial_validation = super(LoginForm, self).validate()
+        if not initial_validation:
+            return False 
+        user = User.query.filter_by(username=self.username.data).first()
+        if not user:
+            self.username.errors.append("Username is not found")
+            return False
+        if user and not bcrypt.check_password_hash(user.password, self.password.data):
+            self.password.errors.append("Password is incorrect")
+            return False
+        return True
--- app/static/script.js
+++ app/static/script.js
@@ -1,3 +1,3 @@
 window.setTimeout(function() {
     $(".alert").fadeTo(500, 0)
-}, 1000);
+}, 2000);
