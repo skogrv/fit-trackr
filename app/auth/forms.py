335ea930984a0ca631f6093a282f62ebfd7573c4
Ihor <skogrv@gmail.com>
2023-05-26 13:56:29 +0300

    refactor: add models button instead of hard-coded

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 from .models import User
 from app import bcrypt
@@ -15,6 +15,7 @@ class RegistrationForm(FlaskForm):
     confirm = PasswordField('Repeat Password', validators=[
                             DataRequired(), EqualTo('password', message='Passwords must match'
                                                                  )])
+    submit = SubmitField()
 
     def validate(self, extra_validators=None):
         initial_validation = super(RegistrationForm, self).validate()
@@ -30,6 +31,7 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log in")
 
     def validate(self, extra_validators=None):
         initial_validation = super(LoginForm, self).validate()
